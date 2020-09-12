c7e6fdc3bb3e3f54b25bf6bb426b6f2c65dc1c93
ajay <ajay199109@gmail.com>
2020-09-13 00:48:26 +0530

    Initial Commit

---
+++ STL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  STL
+//
+//  Created by Ajay Singh on 13/09/20.
+//  Copyright © 2020 Ajay Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
