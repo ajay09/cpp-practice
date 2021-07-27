241aaafb7da0890a2313ba328b4ff8d299daefe1
ajay <ajay199109@gmail.com>
2021-07-27 18:49:30 +0530

    kilo-editor : setup a welcome message.

--- kilo_editor/kilo.c
+++ kilo_editor/kilo.c
@@ -16,6 +16,8 @@
 
 /*** defines ***/
 
+#define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) (0x1f & (k))  // Press Ctrl-Q to quit
 
 
@@ -267,10 +269,23 @@ void editorProcessKeypress() {
 
 // draws a column of tildes on the left hand side of the screen like vim
 void editorDrawRows(struct abuf *ab) {
-    int y;
-    for (y = 0; y < E.screenrows; y++) {
-        abAppend(ab, "~", 1);
-        // write(STDOUT_FILENO, "~", 1);
+    for (int y = 0; y < E.screenrows; y++) {
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                                "Kilo editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+            // write(STDOUT_FILENO, "~", 1);
+        }
 
         abAppend(ab, "\x1b[K", 3);  // The K command erases part of the current line.
                                     // 0 is the default argument, erases the part of the line to the right of the cursor.
